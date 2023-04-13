ea866328510496028ea6e0910c844a8d51f14eb0
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-04-13 00:55:37 -0400

    agregados pokemones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import { Card, Grid, Row, Text } from '@nextui-org/react'
 import type { GetStaticProps, NextPage } from 'next'
-import { Layout } from '../components/layouts'
 import { pokeApi } from '../api'
+import { Layout } from '../components/layouts'
 import { PokemonListResponse, SmallPokemon } from '../interfaces'
 
 interface Props {
@@ -13,24 +14,44 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
 	return (
 		<>
 			<Layout title='Listado de Pokémons'>
-				<ul>
-					<li>Pokemon</li>
-					<li>Pokemon</li>
-					<li>Pokemon</li>
-					<li>Pokemon</li>
-					<li>Pokemon</li>
-					<li>Pokemon</li>
-				</ul>
+				<Grid.Container gap={2} justify='flex-start'>
+					{pokemons.map(({ id, name, img }) => (
+						<Grid xs={6} sm={3} md={2} xl={1} key={id}>
+							<Card isHoverable isPressable>
+								<Card.Body css={{ p: 1 }}>
+									<Card.Image src={img} width={'100%'} height={140} />
+								</Card.Body>
+								<Card.Footer>
+									<Row justify='space-between'>
+										<Text>{name}</Text>
+										<Text>{id}</Text>
+									</Row>
+								</Card.Footer>
+							</Card>
+						</Grid>
+					))}
+				</Grid.Container>
 			</Layout>
 		</>
 	)
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-	const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+	const { data } = await pokeApi.get<PokemonListResponse>('/pokemon/?limit=200&offset=150')
+
+	//Es un arreglo de pokemons que son objetos, por eso el map retorna un objeto
+	let numero = 151
+	const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
+		...poke,
+		id: numero + i,
+		img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+			numero + i
+		}.svg`,
+	}))
+
 	return {
 		props: {
-			pokemons: data.results,
+			pokemons,
 		},
 	}
 }
